ba4a025cd11af7671a34bfcfacb00c320c7dfa44
Katherine Domingo <katherine.domingo@torchbox.com>
2023-10-03 12:37:24 +0800

    Run updatemodulepaths

--- tests/urls.py
+++ tests/urls.py
@@ -1,12 +1,7 @@
 from django.urls import include, path
+from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
-
-try:
-    from wagtail import urls as wagtail_urls
-except ImportError:
-    # Wagtail<3.0
-    from wagtail.core import urls as wagtail_urls
 
 
 urlpatterns = [
