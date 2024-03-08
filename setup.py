e2f7736b8330cc8601884f790c52133df366f4a4
Katherine Domingo <katherine.domingo@torchbox.com>
2024-03-08 10:38:46 +0800

    Remove classifier for Python 3.7

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Site Management',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
